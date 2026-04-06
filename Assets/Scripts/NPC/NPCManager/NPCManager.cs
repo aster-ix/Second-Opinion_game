5a66680dd6a84e55ae2f771d3ecc8732d104ee2d
aster-ix <personal.asterious@gmail.com>
2026-04-06 17:11:16 +0300

    doc

--- Assets/Scripts/NPC/NPCManager/NPCManager.cs
+++ Assets/Scripts/NPC/NPCManager/NPCManager.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 public class NPCManager : MonoBehaviour
 {
     public GameObject DialogUI;
-    public Transform NpcSpawn; 
+    public Transform NpcSpawn;
     public List<GameObject> NPCs = new();
     public int NPCCount = 0;
     public DialogUIManagerScript UIManager;
     public int QuestionNum = 2;
     public int CurrentQuestionNum = 0;
-    
-    [SerializeField] 
+
+    public List<DocumentData> NPCDocuments = new();
+
+   
+    public DocumentPickup caseDocument;
+
+    [SerializeField]
     private GameObject _currentNPC;
+
     void Start()
     {
         UIManager = GameObject.FindGameObjectWithTag("HUD").GetComponent<DialogUIManagerScript>();
@@ -22,22 +28,27 @@ public class NPCManager : MonoBehaviour
     {
         CurrentQuestionNum = 0;
     }
+
     public void NextNPC()
     {
-        
         if (CurrentQuestionNum == QuestionNum) return;
-        if (!DialogUI.activeInHierarchy)  DialogUI.SetActive(true);
+        if (!DialogUI.activeInHierarchy) DialogUI.SetActive(true);
+
         DeleteNPC();
+
         var npc = Instantiate(NPCs[NPCCount], NpcSpawn.transform);
         _currentNPC = npc;
-        
-        UIManager.dialogManager =  npc.GetComponent<DialogManager>();
+
+        UIManager.dialogManager = npc.GetComponent<DialogManager>();
         UIManager.ShowCurrentDialog();
-        //UIManager.NextDialogButton.interactable = false;
-        
+
+        if (caseDocument != null && NPCCount < NPCDocuments.Count && NPCDocuments[NPCCount] != null)
+        {
+            caseDocument.data = NPCDocuments[NPCCount];
+        }
+
         NPCCount++;
         CurrentQuestionNum++;
-        
     }
 
     public void DeleteNPC()
